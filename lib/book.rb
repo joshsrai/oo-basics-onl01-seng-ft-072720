ee8d6baa1264d92841e09891cbb49403da97d074
Josh Raiborde <joshsrai@yahoo.com>
2020-08-02 20:36:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -9,10 +9,18 @@ class Book
   def title
     @title
   end
+  
   def author=(author)
     @author = author
   end
   def author
     @author
+  end
+  
+  def page_count=(page_count)
+    @page_count= page_count
+  end
+  def page_count
+    @page_count
   end
 end
