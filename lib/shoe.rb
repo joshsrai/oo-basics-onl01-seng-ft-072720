b9276c2ead5ee67252a708404fe30702fc7989e4
Josh Raiborde <joshsrai@yahoo.com>
2020-08-02 21:09:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
-class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
   def initialize(title)
     @title = title
   end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+  def cobble
+    puts "repaired"
   end
 
 end
